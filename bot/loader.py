7ce59eb2417b827c289384992a75a0b4507e792b
RoMango <112846459+FlewToL@users.noreply.github.com>
2025-04-10 10:32:57 +0300

    Delete bot directory

--- bot/loader.py
+++
@@ -1,9 +0,0 @@
-from aiogram import Dispatcher, Bot
-from aiogram.client.default import DefaultBotProperties
-
-from config.config_reader import settings
-
-
-bot = Bot(token=settings.BOT_TOKEN.get_secret_value(),
-          default=DefaultBotProperties(parse_mode='html'))
-dp = Dispatcher(bot=bot)
--- bot/src/keyboards/config.py
+++
@@ -1,8 +0,0 @@
-from aiogram.filters.callback_data import CallbackData
-
-CALLBACK_LIFETIME = 3600
-
-
-class GeneralKb(CallbackData, prefix="general"):
-    action: str
-    uuid_key: str
